d20df1da9be3b8bbb29f9cdee76b6ed6bea73135
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-05-24 21:17:15 +0000

    End of class, need to come back and fix code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
 def index
-  @posts = Post.all
+  @posts = Post.order(id desc")
 end
 
 def create
   @post = Post.new
-  @post.body = params["body"]
-  @posts.user_id = @current_user["user_id"]
+  @post["body"] = params[body]
+  @post["user_id"] = @current_user["user_id"]
   @post.save
   redirect_to "/posts"
 end
